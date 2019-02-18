5bd90ceec78b2174795f167848cce370c0e9b0cc
vick3d <viktoro0090@gmail.com>
2019-02-18 09:46:03 +0100

    separates assertions from other test steps, user can visit the landing page

--- features/basic_steps.rb
+++
@@ -1,7 +0,0 @@
-When("I visit the site") do
-    visit "/"
-  end
-  
-  Then("I should see {string}") do |content|
-    expect(page).to have_content content
-  end
--- features/step_definitions/new_objects_step.rb
+++ features/step_definitions/new_objects_step.rb
@@ -1,13 +1,35 @@
 
 
 
-Given("the following articles exists in our database") do |table|
+  Given("the following articles exists in our database") do |table|
     table.hashes.each do |article|
             Article.create!(article)
     end        
+  end 
+
+
+  Given("I visit the {string} page") do |site|
+    case site
+    when "landing"
+      visit root_path
+    else
+      return false
+    end
   end
   
-  Then("I should see Learning Rails {int}") do |int|
+  When("I click {string} link") do |string|
     pending # Write code here that turns the phrase above into concrete actions
   end
+  
+  When("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click {string} button") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+
+  
+  
   
